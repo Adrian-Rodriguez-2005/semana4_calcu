26b405412ee737b565dd3e929ef099d30b9c3361
Adrian-Rodriguez-2005 <166246268+Adrian-Rodriguez-2005@users.noreply.github.com>
2024-04-21 00:08:57 -0500

    Add files via upload

---
+++ Clase4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase4;
+
+import clase4.pantalla.Pantallacalculadora;
+
+/**
+ *
+ * @author arodr
+ */
+public class Clase4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pantallacalculadora pant = new Pantallacalculadora();
+        pant.setVisible(true);
+        
+    }
+    
+}
